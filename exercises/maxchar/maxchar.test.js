95136212c5f6081fabb3190953a11bd8ebfd1e58
emanuele <emanuele.sabbadini@gmail.com>
2020-06-10 23:41:14 +0200

    added exercizes

---
+++ exercises/maxchar/maxchar.test.js
@@ -0,0 +1,14 @@
+const maxChar = require('./index');
+
+test('maxChar function exists', () => {
+  expect(typeof maxChar).toEqual('function');
+});
+
+test('Finds the most frequently used char', () => {
+  expect(maxChar('a')).toEqual('a');
+  expect(maxChar('abcdefghijklmnaaaaa')).toEqual('a');
+});
+
+test('Works with numbers in the string', () => {
+  expect(maxChar('a b1c1       d1e 1 f1g1')).toEqual(' ');
+});
